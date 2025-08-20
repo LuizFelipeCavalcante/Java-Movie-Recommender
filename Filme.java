b6276dd8fa59b30bb942f0784ffa0a6fc383ed44
Luiz Felipe Cavalcante <luizf.a.cavalcante@gmail.com>
2025-08-19 22:08:31 -0300

    classe filme incompleta

---
+++ Filme.java
@@ -0,0 +1,24 @@
+class Filme{
+
+    
+    private String title;
+    private String gender;
+    private int popularity;
+    private int year;
+    private String director;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    
+
+}
